36ed7fd85bd6e407a7998a18fab5d712a3e6598c
Pankaj Ghosh <dev.pankajghosh@gmail.com>
2025-01-02 00:20:49 +0530

    feat: add JavaScript files for basic console logging and data types demonstration

---
+++ js/part1/datatype.js
@@ -0,0 +1,34 @@
+console.log("hello");
+// console.log("world");
+
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefind
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 120;
+let score = 120;
+let name = "Pankaj Ghosh";
+let isLoggedIn = false;
+
+// object
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = {
+  firstname: "pankaj",
+  lastname: "ghosh",
+};
+
+let getScore = score;
+
+console.log(name);
---
+++ js/part1/printing.js
@@ -0,0 +1,5 @@
+console.log("hello");
+process.stdout.write("world\n");
+
+console.table({ city: "kolkata" });
+console.warn({ city: "kolkata" });
