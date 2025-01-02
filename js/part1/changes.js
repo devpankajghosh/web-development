bbd9b9d791aea3360000c71d4374584c7cf930b9
Pankaj Ghosh <dev.pankajghosh@gmail.com>
2025-01-03 00:22:23 +0530

    feat: add JavaScript files for variable declear, logical operations, and number calculations

---
+++ js/part1/changes.js
@@ -0,0 +1,10 @@
+let gameName = "spiderman";
+
+gameName = "batname";
+console.log(gameName);
+
+const username = "iampankajghosh";
+
+username = "pankaj";
+
+console.log(username);
---
+++ js/part1/logical.js
@@ -0,0 +1,13 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedIn = true;
+let isPaid = false;
+
+console.log(isLoggedIn && isPaid); // false
+
+let isEmailUser = true;
+let isGoogleUser = false;
+
+console.log(isEmailUser || isGoogleUser); // true
